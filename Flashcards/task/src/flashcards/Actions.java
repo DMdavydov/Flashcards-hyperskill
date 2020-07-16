d6019092e1de188e80d62f136c4cf6a39e87c226
dmitry_davydov1 <dddavydov@alfabank.ru>
2020-07-16 17:17:01 +0300

    stage 5

--- Flashcards/task/src/flashcards/Actions.java
+++ Flashcards/task/src/flashcards/Actions.java
@@ -1,6 +1,121 @@
 package flashcards;
 
+import java.io.*;
+import java.util.*;
+
 public class Actions {
 
+    protected Scanner scanner = new Scanner(System.in);
+    protected Map<String, String> flashcards = new LinkedHashMap<>();
 
+    public void add() {
+        System.out.println("The card:");
+        String term = scanner.nextLine();
+        if (flashcards.containsKey(term)) {
+            System.out.printf("The card \"%s\" already exists.\n", term);
+            return;
+        }
+
+        System.out.println("The definition of the card:");
+        String def = scanner.nextLine();
+        if (flashcards.containsValue(def)) {
+            System.out.printf("The definition \"%s\" already exists.\n", def);
+            return;
+        }
+
+        flashcards.put(term, def);
+        System.out.println("The pair (\"" + term + "\":\"" + def + "\") has been added.");
+    }
+
+    public void remove() {
+        System.out.println("The card:");
+        String term = scanner.nextLine();
+
+        if (flashcards.remove(term) == null) {
+            System.out.println("Can't remove \"" + term + "\": there is no such card.");
+            return;
+        }
+        System.out.println("The card has been removed.");
+    }
+
+    public void exportCard() {
+        System.out.println("File name:");
+
+        File file = new File(scanner.nextLine());
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            for (var entry : flashcards.entrySet()) {
+                printWriter.println(entry.getKey());
+                printWriter.println(entry.getValue());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(flashcards.size() + " cards have been saved.");
+    }
+
+    public void importCard() {
+        System.out.println("File name:");
+        String fileName = scanner.nextLine();
+
+        int count = 0;
+
+        File file = new File(fileName);
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                flashcards.put(scanner.nextLine(), scanner.nextLine());
+                count++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Not Found");
+        }
+        if (count > 0) {
+            System.out.println(count + " cards have been loaded.");
+        }
+    }
+
+    public void ask() {
+
+        int quantity;
+
+        System.out.println("How many times to ask?");
+        try {
+            quantity = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Wrong input");
+            return;
+        }
+
+        scanner.nextLine();
+        List<String> keys = new ArrayList<>(flashcards.keySet());
+
+        for (int i = 0; i < quantity; i++) {
+            Random random = new Random();
+
+            String randomKey = keys.get(random.nextInt(keys.size()));
+            String randomValue = flashcards.get(randomKey);
+
+            System.out.println("Print definition of \"" + randomKey + "\":");
+            String answer = scanner.nextLine();
+            if (!randomValue.equals(answer) && flashcards.containsValue(answer)) {
+                System.out.println("Wrong answer. The correct one is \"" + randomValue + "\", you've just written the definition of \"" + getKeyFromMap(flashcards, answer) + "\".");
+            } else if (randomValue.equals(answer)) {
+                System.out.println("Correct answer.");
+            } else {
+                System.out.println("Wrong answer. The correct one is \"" + randomValue + "\".");
+            }
+        }
+    }
+
+    public <K, V> K getKeyFromMap(Map<K, V> card, V description) {
+        for (Map.Entry<K, V> entry : card.entrySet()) {
+            if (entry.getValue().equals(description)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public void exit() {
+        System.out.println("Bye bye!");
+    }
 }
--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -4,68 +4,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Actions actions = new Actions();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input the action (add, remove, import, export, ask, exit):");
 
-        switch (scanner.next()) {
-            case "add":
-                break;
-            case "remove":
-                break;
-            case "import":
-                break;
-            case "export":
-                break;
-            case "ask":
-                break;
-            case "exit":
-                break;
-        }
-
-        System.out.println("Input the number of cards:");
-        int n = Integer.parseInt(scanner.nextLine());
-        Map<String, String> flashcards = new LinkedHashMap<>();
-
-        for (int i = 0; i < n; i++) {
-
-            System.out.printf("The card #%d:\n", i + 1);
-            String term = scanner.nextLine();
-            while (flashcards.containsKey(term)) {
-                System.out.printf("The card \"%s\" already exists. Try again:\n", term);
-                term = scanner.nextLine();
+        String action = null;
+        while (!"exit".equals(action)) {
+            System.out.println("Input the action (add, remove, import, export, ask, exit):");
+            action = scanner.next();
+            switch (action) {
+                case "add":
+                    actions.add();
+                    break;
+                case "remove":
+                    actions.remove();
+                    break;
+                case "import":
+                    actions.importCard();
+                    break;
+                case "export":
+                    actions.exportCard();
+                    break;
+                case "ask":
+                    actions.ask();
+                    break;
+                case "exit":
+                    actions.exit();
+                    break;
             }
-
-            System.out.printf("The definition of the card #%d:\n", i + 1);
-            String def = scanner.nextLine();
-            while (flashcards.containsValue(def)) {
-                System.out.printf("The definition \"%s\" already exists. Try again:\n", def);
-                def = scanner.nextLine();
-            }
-
-            flashcards.put(term, def);
-        }
-
-
-        for (String term : flashcards.keySet()) {
-
-            System.out.printf("Print the definition of \"%s\":\n", term);
-            String input = scanner.nextLine();
-
-            if (input.equals(flashcards.get(term))) {
-                System.out.println("Correct answer.");
-            } else if (flashcards.containsValue(input)){
-                System.out.printf("Wrong answer. The correct one is \"%s\",", flashcards.get(term));
-                String otherTerm  = "";
-                for (String s : flashcards.keySet()) {
-                    if (flashcards.get(s).equals(input)) {
-                        otherTerm = s;
-                    }
-                }
-                System.out.printf("you've just written the definition of \"%s\".\n", otherTerm);
-            } else {
-                System.out.printf("Wrong answer. The correct one is \"%s\".\n", flashcards.get(term));
-            }
-
         }
     }
 }
